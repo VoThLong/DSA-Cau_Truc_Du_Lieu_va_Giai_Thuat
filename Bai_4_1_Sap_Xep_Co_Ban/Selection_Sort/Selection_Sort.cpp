14a264df2628f999d20e9864f99ba203235a56d8
Dashtrad <24521025@ms.uit.edu.vn>
2025-07-25 23:58:50 +0700

    Hoàn thành selection sort ở ubuntu

--- Bai_4_1_Sap_Xep_Co_Ban/Selection_Sort/Selection_Sort.cpp
+++ Bai_4_1_Sap_Xep_Co_Ban/Selection_Sort/Selection_Sort.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void swap(int &a, int &b) {
-    int temp = a;
+template <class Object>
+void Swap(Object &a, Object &b) {
+    Object temp = a;
     a = b;
     b = temp;
 }
+template <class Object>
+void SelectionSort(vector<Object> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            Swap(arr[i], arr[minIndex]);
+        }
+    }
+}
 
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+    SelectionSort(arr);
+    
+    cout << "Sorted array: \n";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
